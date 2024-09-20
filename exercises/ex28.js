f3f22c5eac30cadb8886cb9082063c49aaf4253c
Ayaka Ito <a.8.32.noko.info@gmail.com>
2024-09-20 14:57:48 -0700

    exercise28 completed

--- exercises/ex28.js
+++ exercises/ex28.js
@@ -11,3 +11,40 @@ deleteMessage(index) - removes the message corresponding to the index provided
 addFriend() - increases the friend count by 1
 removeFriend() - decreases the friend count by 1
 */
+
+const facebookProfile = {
+  "your name": "",
+  "the number of friends you have": 50,
+  "an array of messages you've posted": ["Hello", "I am a student", "I am from Japan"],
+
+  postMessage: function postMessage(masage) {
+    facebookProfile["an array of messages you've posted"].push(masage);
+  },
+  deleteMessage: function deleteMessage(index) {
+    facebookProfile["an array of messages you've posted"].splice(index, 1);
+  },
+  addFriend: function addFriend() {
+    facebookProfile["the number of friends you have"] =
+      facebookProfile["the number of friends you have"] + 1;
+  },
+  removeFriend: function removeFriend() {
+    facebookProfile["the number of friends you have"] =
+      facebookProfile["the number of friends you have"] - 1;
+  },
+};
+
+facebookProfile["your name"] = "AYAKA ITO";
+facebookProfile["the number of friends you have"] = 50;
+facebookProfile["an array of messages you've posted"] = [
+  "Hello",
+  "I am a student",
+  "I am from Japan",
+];
+facebookProfile.postMessage("I love McDonald's");
+facebookProfile.deleteMessage(1);
+facebookProfile.addFriend();
+facebookProfile.addFriend();
+facebookProfile.addFriend();
+facebookProfile.removeFriend();
+
+console.log(facebookProfile);
